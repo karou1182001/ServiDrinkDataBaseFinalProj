27625532db9f52311dd03ecc6e9a4b752d795daa
María Zapata <54915677+karou1182001@users.noreply.github.com>
2023-04-26 17:05:01 -0400

    Table creation changes

--- client/src/components/userview/savedRestaurants.js
+++ client/src/components/userview/savedRestaurants.js
@@ -58,7 +58,7 @@ const [restaurants, setRestaurants] = useState([]);
         <div className="tm-list">
         {restaurants.map(restaurant => (
         <div className="tm-list-item">
-            <img src={"https://cdn.vox-cdn.com/thumbor/5d_RtADj8ncnVqh-afV3mU-XQv0=/0x0:1600x1067/1200x900/filters:focal(672x406:928x662)/cdn.vox-cdn.com/uploads/chorus_image/image/57698831/51951042270_78ea1e8590_h.7.jpg"} alt="Image" className="tm-list-item-img"/>
+            <img src={restaurant.restImage} alt="Image" className="tm-list-item-img"/>
             <div className="tm-black-bg tm-list-item-text">
             <h3 className="tm-list-item-name"> {restaurant.rname}
             <span className="tm-list-item-price">delete</span></h3>
